20894465b41b0ea25284b23243975d702d42a8c0
toby <toby.bell1702@hotmail.co.uk>
2020-05-08 19:05:02 +0100

    Reworked build.gradle and Added in a test endpoint

--- src/main/java/com/read/AI/AiApplication.java
+++ src/main/java/com/read/AI/AiApplication.java
@@ -2,9 +2,17 @@ package com.read.AI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AiApplication {
+
+	@RequestMapping("/")
+	public String test() {
+		return "Hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiApplication.class, args);
